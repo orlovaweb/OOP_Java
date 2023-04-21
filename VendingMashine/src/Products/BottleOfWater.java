c1f9719543f8cd5c0c4eba231047adfea8d93574
l.orlova <igimon@mail.ru>
2023-04-21 12:10:13 +0300

    set comments

--- VendingMashine/src/Products/BottleOfWater.java
+++ VendingMashine/src/Products/BottleOfWater.java
@@ -1,34 +1,46 @@
 package Products;
 
+/*
+ * Класс Бутылка воды дочерний от Продуктов
+ */
 public class BottleOfWater extends Product {
 
     private int volume;
 
-    public BottleOfWater(String name, double price, int volume)
-    {
+    /*
+     * Перегрузка конструктора
+     * 
+     * @param name название
+     * 
+     * @param price цена
+     * 
+     * @param volume объем бутылки
+     */
+    public BottleOfWater(String name, double price, int volume) {
         super(name, price);
         this.volume = volume;
     }
 
+    /* Получение объема */
     public int getVolume() {
         return volume;
     }
 
+    /* Установить объем */
     public void setVolume(int volume) {
         this.volume = volume;
     }
 
-        /**
+    /**
      * переопределение вывода продукта
      */
     @Override
-    public String toString()
-    {
-        return  "Product{" +
-        "name='" + super.getName() + '\'' +
-        ", cost=" + super.getPrice() +
-        ", volume=" + volume +
-        '}';
+    public String toString() {
+        return "Product{" +
+                "name='" + super.getName() + '\'' +
+                ", cost=" + super.getPrice() +
+                ", volume=" + volume +
+                '}';
     }
-    
+
 }
--- VendingMashine/src/Products/HotDrink.java
+++ VendingMashine/src/Products/HotDrink.java
@@ -1,28 +1,46 @@
 package Products;
 
+/*
+ * Класс Горячие напитки дочерний от Продуктов
+ */
 public class HotDrink extends Product {
 
     private int volume;
     private int temperature;
 
+    /*
+     * Перегрузка конструктора
+     * 
+     * @param name название
+     * 
+     * @param price цена
+     * 
+     * @param volume объем напитка
+     * 
+     * @param tempetature температура напитка
+     */
     public HotDrink(String name, double price, int volume, int temperature) {
         super(name, price);
         this.volume = volume;
         this.temperature = temperature;
     }
 
+    /* Получение объема */
     public int getVolume() {
         return volume;
     }
 
+    /* Получение температуры */
     public int getTemperature() {
         return temperature;
     }
 
+    /* Установить объем */
     public void setVolume(int volume) {
         this.volume = volume;
     }
 
+    /* Установить температуру */
     public void setTemperature(int temperature) {
         this.temperature = temperature;
     }
